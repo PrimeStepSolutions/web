38791373e3bf85d9e0f69fa9dcfd7cc50a920ca2
PrimeStep <primestepsolutions7@gmail.com>
2024-10-20 00:55:25 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/web/', // Set this to the name of your repository
 })
